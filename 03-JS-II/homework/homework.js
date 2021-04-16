319343aa928d81cad064ea06c614ad7d6be5d947
Javier Hernan Martinez Blanco <martinezblanco.jh@gmail.com>
2021-04-16 15:47:24 -0300

    finished homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -118,7 +118,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if(Math.floor(numero) || numero === 0){
+  if(numero === Math.round(numero)){
     return true;
   }else{
     return false;
@@ -169,12 +169,10 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if( numero < 2){
+  if( numero <= 2){
     return false;
   }
-  if(numero === 2){ 
-    return true;
-  }
+  
 
   for(var i = 2; i < numero; i++) {
     if(numero % i === 0) {
@@ -201,11 +199,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  let arrayTablaDel6 = []
-  for (let i = 0; i < 11; i++) {
-        arrayTablaDel6.push(6 * i)
+  let array = []
+  for(var i = 0; i < 11; i++) {
+      array.push(6 * i);
   }
-  return arrayTablaDel6
+  return array;
   
 }
 
@@ -222,14 +220,14 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  var a = numero;
-  var b = 0;
-  do{
-    b++;
-    a = a + 5;
-  }while(b < 8);
+  var i = 0;
 
-  return a;
+  do{
+    i++;
+    numero += 5;
+  }while(i < 8)
+
+  return numero;
 }
 
 
